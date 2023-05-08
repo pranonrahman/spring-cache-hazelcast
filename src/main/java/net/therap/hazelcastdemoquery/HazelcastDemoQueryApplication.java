5f79ef772f1a6a2c3183e27715879fe0257b3a7a
Raian Rahman <raian.rahman@therapservices.net>
2023-05-08 11:49:22 +0600

    initial commit

---
+++ src/main/java/net/therap/hazelcastdemoquery/HazelcastDemoQueryApplication.java
@@ -0,0 +1,13 @@
+package net.therap.hazelcastdemoquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastDemoQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastDemoQueryApplication.class, args);
+	}
+
+}
