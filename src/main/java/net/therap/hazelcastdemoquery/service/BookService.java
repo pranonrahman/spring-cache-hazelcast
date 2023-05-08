c69c33d34e6284f5f61e53535b0a1dd06d2bd5db
Raian Rahman <raian.rahman@therapservices.net>
2023-05-08 18:20:07 +0600

    Add cache

--- src/main/java/net/therap/hazelcastdemoquery/service/BookService.java
+++ src/main/java/net/therap/hazelcastdemoquery/service/BookService.java
@@ -3,6 +3,8 @@ package net.therap.hazelcastdemoquery.service;
 import lombok.RequiredArgsConstructor;
 import net.therap.hazelcastdemoquery.model.Book;
 import net.therap.hazelcastdemoquery.reposiroty.BookRepository;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,10 +19,12 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    @CachePut(value = "books", key = "#book.id")
     public Book save(Book book) {
         return bookRepository.save(book);
     }
 
+    @Cacheable(key = "#id", cacheNames = {"books"})
     public Book findById(Long id) {
         return bookRepository.findById(id).orElse(null);
     }
